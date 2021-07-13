09a43a52049717acac8d29a50213146fe7e6d0d2
alina <alina.bregvadze@gmail.com>
2021-07-13 20:44:54 +0400

    lesson 20

---
+++ script.js
@@ -0,0 +1,27 @@
+let atags = document.querySelectorAll('a');
+
+for (let i = 0; i < atags.length; i++) {
+	atags[i].addEventListener('click', function(e) {
+		// console.log(this.href);
+		
+		let id = this.getAttribute('href').slice(1);
+		// console.log(id);
+		
+		let scroll = document.getElementById(id).offsetTop;
+		e.preventDefault();
+
+		scrollDown(scroll);
+	})
+	
+}
+
+function scrollDown(max) {
+	let scrollTop = document.querySelector('html').scrollTop;
+
+	if(scrollTop < max) {
+		setTimeout(() => {
+			document.querySelector('html').scrollTop += 7;
+			scrollDown(max);
+		}, 2);
+	}
+}
